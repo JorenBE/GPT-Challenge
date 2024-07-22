df3f5682297d0a3e03a79c96f9015df2901ef562
Joren <joren.vanherck@epfl.ch>
2024-07-22 11:13:55 +0200

    added summary plot code

---
+++ experiments/summaryPlot/config.py
@@ -0,0 +1,25 @@
+COLORS = {'Adhesive Energy': 'maroon', 
+          'Properties of Monomers': 'sienna', 
+          'Melting Point': 'darkgoldenrod', 
+          'Viscosity': 'slategray',
+          'Mg Alloys': 'skyblue',
+          'LLPS of Proteins': 'crimson',
+
+          'Cycloaddition': 'forestgreen', 
+          'Catalyzed Cleavage Reaction': 'springgreen',
+          'Catalytic Isomerization': 'royalblue',
+          'Polymerization Kinetics': 'salmon',
+          'Photocatalysis (Water Splitting)': 'indianred', 
+          'Photocatalysis (CO2 Conversion)': 'olivedrab',
+          'MOF Synthesis': 'peru',
+
+          'Gas Uptake/Diffusion of MOFs': 'darkorange', 
+          'Hydrogen Storage Capacity': 'turquoise', 
+          'Biomass-derived Adsorbents': 'teal', 
+          'Thermal Desalination': 'slateblue',
+          'Detection Response of Gas Sensors': 'indigo',
+          'Stability of Gas Sensors': 'plum',
+          'Biomass Gasification': 'violet',
+          'Nanoparticles': 'wheat'
+
+}
