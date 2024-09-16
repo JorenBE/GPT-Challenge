aff82cc8fe1b6518fa96a112ccad711746887b66
Joren <joren.vanherck@epfl.ch>
2024-09-16 14:46:51 +0200

    add turino

---
+++ experiments/01_Materials_and_Properties/fats_turino/local_remote.sh
@@ -0,0 +1,2 @@
+
+rcp -r ./fats_learningCurve joren@128.178.117.168:gptchallengeV2/gptchem-gptj/experiments/
---
+++ experiments/01_Materials_and_Properties/fats_turino/remote_local.sh
@@ -0,0 +1,2 @@
+rcp -r joren@128.178.117.168:gptchallengeV2/gptchem-gptj/experiments/fats_learningCurve/out ./
+
